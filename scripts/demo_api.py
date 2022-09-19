e9ffd147c912d62a2aaa429f6306986ea00db499
Lê Tấn Lộc <ltanloc97o@gmail.com>
2022-09-19 23:42:52 +0700

    nhớ chạy demo_api

--- scripts/demo_api.py
+++ scripts/demo_api.py
@@ -23,46 +23,49 @@ from alphapose.models import builder
 from alphapose.utils.config import update_config
 from detector.apis import get_detector
 from alphapose.utils.vis import getTime
-
+from alphapose.utils.args import args
 """----------------------------- Demo options -----------------------------"""
-parser = argparse.ArgumentParser(description='AlphaPose Single-Image Demo')
-parser.add_argument('--cfg', type=str, required=True,
-                    help='experiment configure file name')
-parser.add_argument('--checkpoint', type=str, required=True,
-                    help='checkpoint file name')
-parser.add_argument('--detector', dest='detector',
-                    help='detector name', default="yolo")
-parser.add_argument('--image', dest='inputimg',
-                    help='image-name', default="")
-parser.add_argument('--save_img', default=False, action='store_true',
-                    help='save result as image')
-parser.add_argument('--vis', default=False, action='store_true',
-                    help='visualize image')
-parser.add_argument('--showbox', default=False, action='store_true',
-                    help='visualize human bbox')
-parser.add_argument('--profile', default=False, action='store_true',
-                    help='add speed profiling at screen output')
-parser.add_argument('--format', type=str,
-                    help='save in the format of cmu or coco or openpose, option: coco/cmu/open')
-parser.add_argument('--min_box_area', type=int, default=0,
-                    help='min box area to filter out')
-parser.add_argument('--eval', dest='eval', default=False, action='store_true',
-                    help='save the result json as coco format, using image index(int) instead of image name(str)')
-parser.add_argument('--gpus', type=str, dest='gpus', default="0",
-                    help='choose which cuda device to use by index and input comma to use multi gpus, e.g. 0,1,2,3. (input -1 for cpu only)')
-parser.add_argument('--flip', default=False, action='store_true',
-                    help='enable flip testing')
-parser.add_argument('--debug', default=False, action='store_true',
-                    help='print detail information')
-parser.add_argument('--vis_fast', dest='vis_fast',
-                    help='use fast rendering', action='store_true', default=False)
-"""----------------------------- Tracking options -----------------------------"""
-parser.add_argument('--pose_flow', dest='pose_flow',
-                    help='track humans in video with PoseFlow', action='store_true', default=False)
-parser.add_argument('--pose_track', dest='pose_track',
-                    help='track humans in video with reid', action='store_true', default=False)
+# parser = argparse.ArgumentParser(description='AlphaPose Single-Image Demo')
+# parser.add_argument('--cfg', type=str, required=True,
+#                     help='experiment configure file name')
+# parser.add_argument('--checkpoint', type=str, required=True,
+#                     help='checkpoint file name')
+# parser.add_argument('--detector', dest='detector',
+#                     help='detector name', default="yolo")
+# parser.add_argument('--image', dest='inputimg',
+#                     help='image-name', default="")
+# parser.add_argument('--save_img', default=False, action='store_true',
+#                     help='save result as image')
+# parser.add_argument('--vis', default=False, action='store_true',
+#                     help='visualize image')
+# parser.add_argument('--showbox', default=False, action='store_true',
+#                     help='visualize human bbox')
+# parser.add_argument('--profile', default=False, action='store_true',
+#                     help='add speed profiling at screen output')
+# parser.add_argument('--format', type=str,
+#                     help='save in the format of cmu or coco or openpose, option: coco/cmu/open')
+# parser.add_argument('--min_box_area', type=int, default=0,
+#                     help='min box area to filter out')
+# parser.add_argument('--eval', dest='eval', default=False, action='store_true',
+#                     help='save the result json as coco format, using image index(int) instead of image name(str)')
+# parser.add_argument('--gpus', type=str, dest='gpus', default="0",
+#                     help='choose which cuda device to use by index and input comma to use multi gpus, e.g. 0,1,2,3. (input -1 for cpu only)')
+# parser.add_argument('--flip', default=False, action='store_true',
+#                     help='enable flip testing')
+# parser.add_argument('--debug', default=False, action='store_true',
+#                     help='print detail information')
+# parser.add_argument('--vis_fast', dest='vis_fast',
+#                     help='use fast rendering', action='store_true', default=False)
+# """----------------------------- Tracking options -----------------------------"""
+# parser.add_argument('--pose_flow', dest='pose_flow',
+#                     help='track humans in video with PoseFlow', action='store_true', default=False)
+# parser.add_argument('--pose_track', dest='pose_track',
+#                     help='track humans in video with reid', action='store_true', default=False)
 
-args = parser.parse_args()
+dirname = os.path.dirname(os.path.dirname(__file__))
+args = args().parse_args(["--cfg",os.path.join(dirname, 'configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml'), 
+                        "--checkpoint", os.path.join(dirname, 'pretrained_models/fast_res50_256x192.pth')
+                        ])
 cfg = update_config(args.cfg)
 
 args.gpus = [int(args.gpus[0])] if torch.cuda.device_count() >= 1 else [-1]
@@ -385,14 +388,14 @@ class SingleImageAlphaPose():
         write_json(final_result, outputpath, form=form, for_eval=for_eval)
         print("Results have been written to json.")
 
-def example():
+def example(image, im_name):
     outputpath = "examples/res/"
     if not os.path.exists(outputpath + '/vis'):
         os.mkdir(outputpath + '/vis')
 
     demo = SingleImageAlphaPose(args, cfg)
-    im_name = args.inputimg    # the path to the target image
-    image = cv2.cvtColor(cv2.imread(im_name), cv2.COLOR_BGR2RGB)
+    # im_name = args.inputimg    # the path to the target image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     pose = demo.process(im_name, image)
     img = demo.getImg()     # or you can just use: img = cv2.imread(image)
     img = demo.vis(img, pose)   # visulize the pose result
--- scripts/demo_inference.py
+++ scripts/demo_inference.py
@@ -22,74 +22,69 @@ from alphapose.utils.transforms import flip, flip_heatmap
 from alphapose.utils.vis import getTime
 from alphapose.utils.webcam_detector import WebCamDetectionLoader
 from alphapose.utils.writer import DataWriter
-from alphapose.utils.args import args
 
 """----------------------------- Demo options -----------------------------"""
-# parser = argparse.ArgumentParser(description='AlphaPose Demo')
-# parser.add_argument('--cfg', type=str, required=True,
-#                     help='experiment configure file name')
-# parser.add_argument('--checkpoint', type=str, required=True,
-#                     help='checkpoint file name')
-# parser.add_argument('--sp', default=False, action='store_true',
-#                     help='Use single process for pytorch')
-# parser.add_argument('--detector', dest='detector',
-#                     help='detector name', default="yolo")
-# parser.add_argument('--detfile', dest='detfile',
-#                     help='detection result file', default="")
-# parser.add_argument('--indir', dest='inputpath',
-#                     help='image-directory', default="")
-# parser.add_argument('--list', dest='inputlist',
-#                     help='image-list', default="")
-# parser.add_argument('--image', dest='inputimg',
-#                     help='image-name', default="")
-# parser.add_argument('--outdir', dest='outputpath',
-#                     help='output-directory', default="examples/res/")
-# parser.add_argument('--save_img', default=False, action='store_true',
-#                     help='save result as image')
-# parser.add_argument('--vis', default=False, action='store_true',
-#                     help='visualize image')
-# parser.add_argument('--showbox', default=False, action='store_true',
-#                     help='visualize human bbox')
-# parser.add_argument('--profile', default=False, action='store_true',
-#                     help='add speed profiling at screen output')
-# parser.add_argument('--format', type=str,
-#                     help='save in the format of cmu or coco or openpose, option: coco/cmu/open')
-# parser.add_argument('--min_box_area', type=int, default=0,
-#                     help='min box area to filter out')
-# parser.add_argument('--detbatch', type=int, default=5,
-#                     help='detection batch size PER GPU')
-# parser.add_argument('--posebatch', type=int, default=64,
-#                     help='pose estimation maximum batch size PER GPU')
-# parser.add_argument('--eval', dest='eval', default=False, action='store_true',
-#                     help='save the result json as coco format, using image index(int) instead of image name(str)')
-# parser.add_argument('--gpus', type=str, dest='gpus', default="0",
-#                     help='choose which cuda device to use by index and input comma to use multi gpus, e.g. 0,1,2,3. (input -1 for cpu only)')
-# parser.add_argument('--qsize', type=int, dest='qsize', default=1024,
-#                     help='the length of result buffer, where reducing it will lower requirement of cpu memory')
-# parser.add_argument('--flip', default=False, action='store_true',
-#                     help='enable flip testing')
-# parser.add_argument('--debug', default=False, action='store_true',
-#                     help='print detail information')
-# """----------------------------- Video options -----------------------------"""
-# parser.add_argument('--video', dest='video',
-#                     help='video-name', default="")
-# parser.add_argument('--webcam', dest='webcam', type=int,
-#                     help='webcam number', default=-1)
-# parser.add_argument('--save_video', dest='save_video',
-#                     help='whether to save rendered video', default=False, action='store_true')
-# parser.add_argument('--vis_fast', dest='vis_fast',
-#                     help='use fast rendering', action='store_true', default=False)
-# """----------------------------- Tracking options -----------------------------"""
-# parser.add_argument('--pose_flow', dest='pose_flow',
-#                     help='track humans in video with PoseFlow', action='store_true', default=False)
-# parser.add_argument('--pose_track', dest='pose_track',
-#                     help='track humans in video with reid', action='store_true', default=False)
+parser = argparse.ArgumentParser(description='AlphaPose Demo')
+parser.add_argument('--cfg', type=str, required=True,
+                    help='experiment configure file name')
+parser.add_argument('--checkpoint', type=str, required=True,
+                    help='checkpoint file name')
+parser.add_argument('--sp', default=False, action='store_true',
+                    help='Use single process for pytorch')
+parser.add_argument('--detector', dest='detector',
+                    help='detector name', default="yolo")
+parser.add_argument('--detfile', dest='detfile',
+                    help='detection result file', default="")
+parser.add_argument('--indir', dest='inputpath',
+                    help='image-directory', default="")
+parser.add_argument('--list', dest='inputlist',
+                    help='image-list', default="")
+parser.add_argument('--image', dest='inputimg',
+                    help='image-name', default="")
+parser.add_argument('--outdir', dest='outputpath',
+                    help='output-directory', default="examples/res/")
+parser.add_argument('--save_img', default=False, action='store_true',
+                    help='save result as image')
+parser.add_argument('--vis', default=False, action='store_true',
+                    help='visualize image')
+parser.add_argument('--showbox', default=False, action='store_true',
+                    help='visualize human bbox')
+parser.add_argument('--profile', default=False, action='store_true',
+                    help='add speed profiling at screen output')
+parser.add_argument('--format', type=str,
+                    help='save in the format of cmu or coco or openpose, option: coco/cmu/open')
+parser.add_argument('--min_box_area', type=int, default=0,
+                    help='min box area to filter out')
+parser.add_argument('--detbatch', type=int, default=5,
+                    help='detection batch size PER GPU')
+parser.add_argument('--posebatch', type=int, default=64,
+                    help='pose estimation maximum batch size PER GPU')
+parser.add_argument('--eval', dest='eval', default=False, action='store_true',
+                    help='save the result json as coco format, using image index(int) instead of image name(str)')
+parser.add_argument('--gpus', type=str, dest='gpus', default="0",
+                    help='choose which cuda device to use by index and input comma to use multi gpus, e.g. 0,1,2,3. (input -1 for cpu only)')
+parser.add_argument('--qsize', type=int, dest='qsize', default=1024,
+                    help='the length of result buffer, where reducing it will lower requirement of cpu memory')
+parser.add_argument('--flip', default=False, action='store_true',
+                    help='enable flip testing')
+parser.add_argument('--debug', default=False, action='store_true',
+                    help='print detail information')
+"""----------------------------- Video options -----------------------------"""
+parser.add_argument('--video', dest='video',
+                    help='video-name', default="")
+parser.add_argument('--webcam', dest='webcam', type=int,
+                    help='webcam number', default=-1)
+parser.add_argument('--save_video', dest='save_video',
+                    help='whether to save rendered video', default=False, action='store_true')
+parser.add_argument('--vis_fast', dest='vis_fast',
+                    help='use fast rendering', action='store_true', default=False)
+"""----------------------------- Tracking options -----------------------------"""
+parser.add_argument('--pose_flow', dest='pose_flow',
+                    help='track humans in video with PoseFlow', action='store_true', default=False)
+parser.add_argument('--pose_track', dest='pose_track',
+                    help='track humans in video with reid', action='store_true', default=False)
 
-dirname = os.path.dirname(os.path.dirname(__file__))
-args = args().parse_args(["--cfg",os.path.join(dirname, 'configs/coco/resnet/256x192_res50_lr1e-3_1x.yaml'), 
-                        "--checkpoint", os.path.join(dirname, 'pretrained_models/fast_res50_256x192.pth'),
-                        "----indir", os.path.join(dirname, 'data')
-                        ])
+args = parser.parse_args()
 cfg = update_config(args.cfg)
 
 if platform.system() == 'Windows':
@@ -164,7 +159,7 @@ def loop():
         n += 1
 
 
-def extractAlphaPose():
+if __name__ == "__main__":
     mode, input_source = check_input()
 
     if not os.path.exists(args.outputpath):
@@ -178,7 +173,7 @@ def extractAlphaPose():
         det_loader = FileDetectionLoader(input_source, cfg, args)
         det_worker = det_loader.start()
     else:
-        det_loader = DetectionLoader(input_source, get_detector(args), cfg, args,batchSize=args.detbatch, mode=mode, queueSize=args.qsize)
+        det_loader = DetectionLoader(input_source, get_detector(args), cfg, args, batchSize=args.detbatch, mode=mode, queueSize=args.qsize)
         det_worker = det_loader.start()
 
     # Load pose model
@@ -299,4 +294,3 @@ def extractAlphaPose():
             writer.terminate()
             writer.clear_queues()
             det_loader.clear_queues()
-
